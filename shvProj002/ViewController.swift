ace9ecd6e62f6dbd6beee2909651d00c946c3305
shv yac <shvyac@gmail.com>
2017-12-22 18:15:21 +0900

    Initial Commit

---
+++ shvProj002/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shvProj002
+//
+//  Created by shvyac on 2017/12/22.
+//  Copyright © 2017年 shvyac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
